1217fdb34ff8db3720fb5cfbf8b4bc23fef1cc74
kvtvs <kata.sara-aho@metropolia.fi>
2021-11-01 15:44:43 +0200

    first commit

---
+++ routes/catRoute.js
@@ -0,0 +1,23 @@
+'use strict';
+// catRoute
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
+const { cat_list_get, cat_get, cat_post } = require('../controllers/catController');
+const router = express.Router();
+
+router.get('/', cat_list_get); 
+
+router.get('/:id', cat_get);
+
+router.post('/', upload.single('cat'), cat_post);
+
+router.put('/', (req, res) => {
+  res.send('With this endpoint you can edit cats.')
+});
+
+router.delete('/', (req, res) => {
+  res.send('With this endpoint you can delete cats.')
+});
+
+module.exports = router;
